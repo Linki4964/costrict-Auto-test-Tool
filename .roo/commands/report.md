17b5e74fe16ad24caee03f0cff3dcc17ed8e7333
Linki4964 <Frozenki4964@gmail.com>
2025-12-15 10:05:06 +0800

    完成前端登录监测脚本的编写

--- .roo/commands/report.md
+++ .roo/commands/report.md
@@ -2,4 +2,6 @@
 description: "你需要生成一个测试报告以markdown的形式进行编写，依照目录下的test-report.md文件作为format prompt模板进行编写"
 ---
 
-这是一个新的斜杠命令。编辑此文件以自定义命令行为。
+- 
+- 
+- 
---
+++ 前端测试脚本/test.py
@@ -0,0 +1,19 @@
+import re
+from playwright.async_api import *
+
+#pip install pytest-playwright在这个进行安装
+
+def test_has_title(page: Page):
+    page.goto("https://playwright.net.cn/")
+
+    # Expect a title "to contain" a substring.
+    expect(page).to_have_title(re.compile("Playwright"))
+
+def test_get_started_link(page: Page):
+    page.goto("https://playwright.net.cn/")
+
+    # Click the get started link.
+    page.get_by_role("link", name="Get started").click()
+
+    # Expects page to have a heading with the name of Installation.
+    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
